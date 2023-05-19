cc70738efae4ed31f92f52358b9e8e703ab9c2b9
IdanDanino <idan25100@gmail.com>
2023-05-19 15:11:50 +0300

    set up theme

---
+++ src/theme/typography.ts
@@ -0,0 +1,55 @@
+import { TypographyOptions } from "@mui/material/styles/createTypography";
+
+const typography: TypographyOptions = {
+  fontFamily: "Nunito",
+  h1: {
+    fontSize: "65px",
+    fontStyle: "italic",
+    fontWeight: 700,
+    letterSpacing: "0em",
+  },
+  h2: {
+    fontSize: "45px",
+    fontStyle: "italic",
+    fontWeight: 600,
+    letterSpacing: "0em",
+  },
+  body1: {
+    fontSize: "30px",
+    fontStyle: "italic",
+    fontWeight: 600,
+  },
+  body2: {
+    fontSize: "20px",
+    fontStyle: "italic",
+    fontWeight: 500,
+  },
+  subtitle1: {
+    fontSize: "30px",
+    fontStyle: "italic",
+    fontWeight: 600,
+  },
+  subtitle2: {
+    fontSize: "25px",
+    fontStyle: "italic",
+    fontWeight: 600,
+  },
+  placeholder: {
+    fontSize: "25px",
+    fontStyle: "italic",
+    fontWeight: 600,
+  },
+  button: {
+    fontSize: "25px",
+    fontStyle: "italic",
+    fontWeight: 700,
+  },
+  caption: undefined,
+  h3: undefined,
+  h4: undefined,
+  h5: undefined,
+  h6: undefined,
+  overline: undefined,
+};
+
+export default typography;
